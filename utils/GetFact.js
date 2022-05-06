6694c91b959fc4a1d7284495e3d1df41a5e3a609
INFI <73887147+INFI-Github@users.noreply.github.com>
2022-05-06 18:46:13 +0530

    Update GetFact.js

--- utils/GetFact.js
+++ utils/GetFact.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const getFact = async(limit) => {
     return new Promise((resolve, reject) => {
-      headers = {headers: {"X-Api-Key": "urwyNY0uNoS99Ix9fWol+A==PGVyMNkUiUP9oNci"}}
+      headers = {headers: {"X-Api-Key": "API KEY HERE"}}
       url = "https://api.api-ninjas.com/v1/facts?limit=" + limit
       axios.get(url , headers).then(res => {
       return resolve(res.data)
