45dcef5e221db7019452b88dfe08c9c970b5e6f0
INFI <73887147+INFI-Github@users.noreply.github.com>
2022-06-19 11:32:12 +0530

    Update daily-facts.js

--- daily-facts.js
+++ daily-facts.js
@@ -3,8 +3,8 @@ const client = require('./utils/Client.js');
 const GetFact = require('./utils/GetFact.js');
 const PostTweet = require('./utils/PostTweet.js');
 
+var day_counter = 1
 const DailyTweetCronJob = cron.job('0 0 * * *', async () => {
-    var day_counter = 1
     const facts = await GetFact(1);
     const tweet_data = facts[0].fact;
     await PostTweet(`Daily Facts , Day ${day_counter} : \n` + tweet_data).then(day_counter++);
